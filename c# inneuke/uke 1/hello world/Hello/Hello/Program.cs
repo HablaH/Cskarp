fe956a5637df3069f395e27330b9e2d3caf08c47
Harald S <harald@getacademy.no>
2020-12-07 12:05:47 +0100

    hello world + bokstav teller

---
+++ c# inneuke/uke 1/hello world/Hello/Hello/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //deklarere variabler
+            bool b;
+            int n;
+            float f;
+            double d;
+            string s;
+
+            //tilordne verdier
+            b = true;
+            n = 5;
+            f = 3.2f;
+            d = 1.2;
+            s = "qwerty";
+
+            //deklarere og tilordne
+            bool b2 = false;
+            int n2 = 82;
+            float f2 = 5.24f;
+            double d2 = 9.357;
+            string s2 = "Lorem Ipsum osv.";
+
+            var b3 = false;
+            var n3 = 29;
+            var f3 = 1.14f;
+            var d3 = 3.14;
+            var s3 = "Ibsens ripsbusker og andre buskvekster";
+
+
+
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Hello World! ingen parametere");
+            }
+            else
+            {
+                Console.WriteLine("Hello World! Parameters: " + args.Length);
+            }
+
+            for (var i = 0; i < args.Length; i++)
+            {
+                var parameterNo = i + 1;
+                Console.WriteLine("Parameter " + parameterNo + ": " + args[i]);
+
+                foreach (var text in args)
+                {
+                    Console.WriteLine("Parameter: " + text);
+                }
+            }
+        }
+    }
+}
---
+++ c# inneuke/uke 1/hvaGjørKoden1/hvaGjørKoden1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hvaGjørKoden1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var range = 250;
+            var counts = new int[range];
+            int sumOfChars = 0;
+            string text = "something";
+            while (!string.IsNullOrWhiteSpace(text))
+            {                
+                text = Console.ReadLine();
+                foreach (var character in text.ToLower() ?? string.Empty)
+                {
+                    Console.WriteLine((int)character);
+                    counts[(int)character]++;
+                    sumOfChars++;
+                }
+                for (var i = 0; i < range; i++)
+                {
+                    if (counts[i] > 0)
+                    {                       
+                        var character = (char)i;
+                        var percentage = 100 * (double)counts[i] / sumOfChars;
+                        string output = character + " - " + counts[i] +" of " + sumOfChars +"  "+ percentage +"%";
+                        Console.CursorLeft = Console.BufferWidth - output.Length - 1;
+                        Console.WriteLine(output);
+                    }
+                }                
+            }
+        }
+    }
+}
